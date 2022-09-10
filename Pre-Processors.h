d18a75ad902de7fc491be029d0926770d9d92961
Nehal Ghewade <nehalghewade18@gmail.com>
2022-09-10 12:56:28 +0530

    Add files via upload

---
+++ Pre-Processors.h
@@ -0,0 +1,27 @@
+/** All Required define Pre-Processors**/
+#define ll long long
+#define ull unsigned long long
+#define scd(t) scanf("%d",&t)
+#define scld(t) scanf("%ld",&t)
+#define sclld(t) scanf("%lld",&t)
+#define scc(t) scanf("%c",&t)
+#define scs(t) scanf("%s",t)
+#define scf(t) scanf("%f",&t)
+#define sclf(t) scanf("%lf",&t)
+#define mem(a, b) memset(a, (b), sizeof(a))
+#define for(i, j, k, in) for (int i=j ; i<k ; i+=in)
+#define rfor(i, j, k, in) for (int i=j ; i>=k ; i-=in)
+#define rep(i, j) for(i, 0, j, 1)
+#define rrep(i, j) rfor(i, j, 0, 1)
+#define all(cont) cont.begin(), cont.end()
+#define rall(cont) cont.end(), cont.begin()
+#define foreach(it, l) for (auto it = l.begin(); it != l.end(); it++)
+#define in(A, B, C) assert( B <= A && A <= C)
+#define mp make_pair
+#define pb push_back
+#define inf (int)1e9
+#define eps 1e-9
+#define pi 3.1415926535897932384626433832795
+#define mod 1000000007
+#define read(type) readInt<type>()
+#define sp ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
